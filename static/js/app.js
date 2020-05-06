0520d5d3bda5f1557deb2c71154f98ae9a8964d0
Elisabeth <elisabethvirak@gmail.com>
2020-05-05 20:24:25 -0400

    Added table to website

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,16 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+console.log(tableData)
+
+data.forEach(function(tableData) {
+  console.log(tableData);
+  var row = tbody.append("tr");
+  Object.entries(tableData).forEach(function([key, value]) {
+    console.log(key, value);
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
