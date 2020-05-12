784465ca4872b2c39ea7bcd2f4f8fcdddf7bedb3
Elisabeth <elisabethvirak@gmail.com>
2020-05-12 11:53:37 -0400

    Fixed filter and added error message

--- static/js/app.js
+++ static/js/app.js
@@ -2,38 +2,60 @@
 var tableData = data;
 
 // select  position in html code where we want the table to be placed
-var tbody = d3.select("tbody");
+var tbody = d3.select('tbody');
 
-// iterate through the table, creating a new table row for each data row
-data.forEach(function(tableData) {
-  console.log(tableData);
-  var row = tbody.append("tr");
-  Object.entries(tableData).forEach(function([key, value]) {
-    console.log(key, value);
-    var cell = row.append("td");
-    cell.text(value);
+// create function to iterate through the table, creating a new table row for each data row
+function displayData(data) {
+
+  // Clear existing data
+  tbody.html("");
+
+  // iterate through data.js and print as html table
+  data.forEach((value) => {
+    var row = tbody.append('tr');
+    Object.entries(value).forEach(([key, value]) => {
+      var cell = row.append('td').text(value);
+    });
   });
-});
+}
+
+// Display tableData when page loads
+displayData(tableData)
 
 // select button id from html
 var button = d3.select('#filter-btn');
 // Select form used for entering date to filter
 var form = d3.select("#form");
 
-// Create event handlers for button
+// Create event handlers
 button.on('click', runEnter);
 form.on('submit', runEnter);
 
 // Create function to handle the events
 function runEnter() {
-  d3.event.preventDefault();
+
   // Prevent the page from refreshing
-  var dateInput = d3.select('#datetime');
+  d3.event.preventDefault();
+
   // Define variable for input date and select id that stores the date
-  var valueDateInput = dateInput.property('value');
-  console.log(valueDateInput);
-  // Define variable for value property of input date
-  var filterTable = tableData.filter(info => info.datetime === valueDateInput);
+  var dateInput = d3.select('#datetime').property('value');
+  console.log(dateInput);
+
+  // Check if dateInput exists and save as an object
+  var filterTable = tableData.filter(row => row.datetime===dateInput);
   console.log(filterTable);
-  filterTable.map()
+
+  // Display desired data
+  displayData(filterTable);
+
+  // If there is no matching data, print error message
+  // Below code thanks to help from Brian
+  if (filterTable.length == 0) {
+    console.log('There is no data matching your search.')
+    d3.select('tbody')
+    .append('tr')
+    .append('td')
+      .attr('colspan', 7)
+      .html('<h4>There is no data matching your search.</h4>');
+  }
 }
